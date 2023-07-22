06e7984d00527ba8c751dc839c9e25ad5bbc3c67
Ryota-Suzki <ryota.business1211@gmail.com>
2023-07-22 21:10:54 +0900

    SECRET_KEY更新と外部ファイルに配置

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .settings_local import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*l7x2nul7x3f)@fs)i%i!g&-wag@9mrf$*^s)c=%5_n#b9w&#0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
